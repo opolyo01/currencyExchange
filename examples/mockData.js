18e2c8a58fea14fb9627f642d3d0e6614c5490e3
Oleg Polyakov <opolyo01@yahoo.com>
2014-10-14 11:24:35 -0700

    added UI

---
+++ examples/mockData.js
@@ -0,0 +1,49 @@
+exports.transactionHistory = [{
+    date: "01/10/2014",
+    title: "Purchase: Macys",
+    price: "99.99",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "01/02/2014",
+    title: "Purchase: Home Depot",
+    price: "150.15",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "12/31/2013",
+    title: "Transfer: Wells Fargo",
+    price: "350.00",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "12/15/2013",
+    title: "Purchase: Jamba Juice",
+    price: "2.30",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "11/01/2013",
+    title: "Refund: DSW shoes",
+    price: "75.45",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "10/20/2013",
+    title: "Purchase: DSW shoes",
+    price: "75.45",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "10/05/2013",
+    title: "Purchase: Jamba Juice",
+    price: "99.99",
+    currency: "USD",
+    symbol: "$"
+},{
+    date: "10/02/2013",
+    title: "Transfer: Wells Fargo",
+    price: "2.75",
+    currency: "USD",
+    symbol: "$"
+}];
--- examples/server.js
+++ examples/server.js
@@ -1,18 +1,25 @@
 var currencyExchange = require('currencyExchange');
+var mockData = require('./mockData');
 var express = require('express');
+var compression = require('compression');
 var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
+var oneDay = 86400000;
 
 //Routes
 var activityRoute = router.route('/activity');
 var currencyConversionRoute = router.route('/currencyConversion');
 var conversionRateRoute = router.route('/conversionRate');
+var transactionHistoryRoute = router.route('/transactionHistory');
 
+app.use(compression());
 app.use('/paypal', router);
 
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
 activityRoute.get(function(req, res) {
-     res.json({ message: 'activityRoute' });
+     res.sendfile(__dirname + '/public/index.html');
 });
 
 currencyConversionRoute.get(function(req, res) {
@@ -21,6 +28,10 @@ currencyConversionRoute.get(function(req, res) {
 
 conversionRateRoute.get(function(req, res) {
      res.json({ message: 'conversionRateRoute' });
+});
+
+transactionHistoryRoute.get(function(req, res) {
+    res.json(mockData.transactionHistory);
 });
 
 // Start the server
